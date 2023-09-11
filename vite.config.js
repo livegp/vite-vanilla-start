625c509d1e670b5290d4e7d9fa8b1bf5b33c0c5d
Oleksandr <live.gp@gmail.com>
2023-09-11 22:10:51 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import { reactClickToComponent } from "vite-plugin-react-click-to-component";
-import eslintPlugin from "@nabla/vite-plugin-eslint";
-import stylelint from "vite-plugin-stylelint";
-import injectHTML from "vite-plugin-html-inject";
-import { ViteMinifyPlugin } from "vite-plugin-minify";
-import webfontDownload from "vite-plugin-webfont-dl";
-import imagePresets, { widthPreset } from "vite-plugin-image-presets";
-import VitePluginSvgSpritemap from "@spiriit/vite-plugin-svg-spritemap";
-import zipPack from "vite-plugin-zip-pack";
+import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+// import { reactClickToComponent } from 'vite-plugin-react-click-to-component';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
+import stylelint from 'vite-plugin-stylelint';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+// import webfontDownload from 'vite-plugin-webfont-dl';
+import imagePresets, { widthPreset } from 'vite-plugin-image-presets';
+import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
+import zipPack from 'vite-plugin-zip-pack';
 // import FullReload from 'vite-plugin-full-reload';
 // import autoAlias from 'vite-plugin-auto-alias';
 // import Icons from 'unplugin-icons/vite';
@@ -21,13 +21,13 @@ export default defineConfig({
     eslintPlugin(),
     stylelint({ fix: true }),
     ViteMinifyPlugin({}),
-    VitePluginSvgSpritemap("./src/icons/*.svg"),
-    webfontDownload(),
+    VitePluginSvgSpritemap('./src/icons/*.svg'),
+    // webfontDownload(),
     zipPack(),
     imagePresets({
       thumbnail: widthPreset({
-        class: "img thumb",
-        loading: "lazy",
+        class: 'img thumb',
+        loading: 'lazy',
         widths: [48, 96],
         formats: {
           webp: { quality: 50 },
@@ -40,5 +40,5 @@ export default defineConfig({
     // autoAlias({ mode: 'sync' }),
     // Icons()
   ],
-  base: "/vite-vanilla-start/", // має відповідати шляху в index.jsx
+  base: '/vite-vanilla-start/', // має відповідати шляху в index.jsx
 });
